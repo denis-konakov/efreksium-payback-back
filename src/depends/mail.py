561f773bb92cd2b52ee3d315cec9191cc837dc6a
Svetovoyvv <svetovoi@sfedu.ru>
2023-03-22 20:00:39 +0300

    add mail error logger

--- src/depends/mail.py
+++ src/depends/mail.py
@@ -2,13 +2,15 @@ from fastapi import BackgroundTasks
 from mail import MailManager
 from utils.throws import throws
 from crud.exceptions import EmailSendMessageException
+from loguru import logger
 @throws([
     EmailSendMessageException
 ])
 def get_mail(bt: BackgroundTasks) -> MailManager:
     try:
         yield MailManager(background_tasks=bt)
-    except Exception:
+    except Exception as e:
+        logger.error('email interaction exception: {}', e)
         raise EmailSendMessageException.get()
 __all__ = (
     'get_mail',
